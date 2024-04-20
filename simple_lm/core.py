68ba46a0a86de4c69591207af0ad280bd457e884
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-20 01:00:40 -0400

    Update version and add new client mode

--- simple_lm/core.py
+++ simple_lm/core.py
@@ -3,6 +3,11 @@ from anthropic import Anthropic
 from openai import OpenAI
 
 
+from instructor import from_openai, from_anthropic, Mode
+from anthropic import Anthropic
+from openai import OpenAI
+
+
 class SimpleLM:
     def __init__(self):
         self.clients = {}
@@ -12,7 +17,9 @@ class SimpleLM:
         self,
         client_name,
         api_key=None,
+        mode=Mode.MD_JSON
     ):
+        
         client_creator = {
             "openai": lambda: from_openai(
                 OpenAI(api_key=api_key),
@@ -20,11 +27,11 @@ class SimpleLM:
             "anthropic": lambda: from_anthropic(Anthropic(api_key=api_key)),
             "together": lambda: from_openai(
                 OpenAI(api_key=api_key, base_url="https://api.together.xyz/v1"),
-                mode=Mode.MD_JSON,
+                mode=mode
             ),
             "ollama": lambda: from_openai(
                 OpenAI(api_key=api_key, base_url="http://localhost:11434/v1"),
-                mode=Mode.TOOLS,
+                mode=mode
             ),
         }
 
@@ -36,6 +43,15 @@ class SimpleLM:
         self.clients[client_name] = client
         
         return client
+
+    def get_client(self, client_name):
+        if client_name in self.clients:
+            return self.clients[client_name]
+        else:
+            raise ValueError(
+                f"Client '{client_name}' not initialized. Please set up the client first."
+            )
+
 
     def get_client(self, client_name):
         if client_name in self.clients:
